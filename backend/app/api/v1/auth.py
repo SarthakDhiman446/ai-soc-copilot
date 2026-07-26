82268b99984ecc16bf92545a027a40ede9c951c0
SarthakDhiman446 <sarthakdhiman904@gmail.com>
2026-07-26 18:11:20 +0530

    feat: implement JWT authentication

--- backend/app/api/v1/auth.py
+++ backend/app/api/v1/auth.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
+from app.auth.dependencies import get_current_user
+from app.models.user import User
 from app.database.database import get_db
-from app.schemas.user import UserCreate, UserResponse, UserLogin
+from app.schemas.user import UserCreate, UserResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from app.schemas.token import Token
 from app.services.user_service import UserService
 from app.auth.jwt_handler import create_access_token
@@ -36,13 +38,13 @@ def register(
     response_model=Token
 )
 def login(
-    user: UserLogin,
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
     db_user = UserService.authenticate_user(
         db,
-        user.email,
-        user.password
+        form_data.username,
+        form_data.password
     )
 
     if not db_user:
@@ -61,3 +63,12 @@ def login(
         "access_token": token,
         "token_type": "bearer"
     }
+
+@router.get(
+    "/me",
+    response_model=UserResponse
+)
+def get_me(
+    current_user: User = Depends(get_current_user)
+):
+    return current_user
